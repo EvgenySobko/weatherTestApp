1309bc31af999a8cae77bd658c3687d3e25f4379
EvgenySobko <31846375+EvgenySobko@users.noreply.github.com>
2019-12-13 18:28:59 +0300

    Update ReactivePresenter.kt

--- app/src/main/java/com/example/weathertestapp/extensions/ReactivePresenter.kt
+++ app/src/main/java/com/example/weathertestapp/extensions/ReactivePresenter.kt
@@ -24,15 +24,6 @@ abstract class ReactivePresenter<V : MvpView> : MvpPresenter<V>() {
         globalDisposable.add(this)
     }
 
-    protected fun <T> Observable<T>.asyncQuery(
-        subscribeObservable: Scheduler = Schedulers.io(),
-        observeScheduler: Scheduler = AndroidSchedulers.mainThread()
-    ): Observable<T> {
-        return this
-            .subscribeOn(subscribeObservable)
-            .observeOn(observeScheduler)
-    }
-
     protected fun <T> Single<T>.asyncQuery(
         subscribeObservable: Scheduler = Schedulers.io(),
         observeScheduler: Scheduler = AndroidSchedulers.mainThread()
@@ -42,33 +33,10 @@ abstract class ReactivePresenter<V : MvpView> : MvpPresenter<V>() {
             .observeOn(observeScheduler)
     }
 
-    protected fun Completable.asyncQuery(
-        subscribeObservable: Scheduler = Schedulers.io(),
-        observeScheduler: Scheduler = AndroidSchedulers.mainThread()
-    ): Completable {
-        return this
-            .subscribeOn(subscribeObservable)
-            .observeOn(observeScheduler)
-    }
-
-    protected fun <T> Observable<T>.bindProgress(onProgress: (inProgress: Boolean) -> Unit): Observable<T> {
-        return this
-            .doOnSubscribe { onProgress.invoke(true) }
-            .doOnNext { onProgress.invoke(false) }
-            .doOnError { onProgress.invoke(false) }
-    }
-
     protected fun <T> Single<T>.bindProgress(onProgress: (inProgress: Boolean) -> Unit): Single<T> {
         return this
             .doOnSubscribe { onProgress.invoke(true) }
             .doOnSuccess { onProgress.invoke(false) }
-            .doOnError { onProgress.invoke(false) }
-    }
-
-    protected fun Completable.bindProgress(onProgress: (inProgress: Boolean) -> Unit): Completable {
-        return this
-            .doOnSubscribe { onProgress.invoke(true) }
-            .doOnComplete { onProgress.invoke(false) }
             .doOnError { onProgress.invoke(false) }
     }
 }
